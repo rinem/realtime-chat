99ada6b766a861d2622aceefab485da9a6cdec57
rinem <roshanmishrarinem@gmail.com>
2019-04-07 17:59:09 +0530

    Update stylesheets

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect(window.location.hostname);
+var socket = io.connect('http://localhost:4000');
 
 var message = document.getElementById('message'),
     username = document.getElementById('username'),
